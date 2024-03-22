f413774f876b9edb798ba0d3fb230f52f6ff1eb2
Abdellahsyani <syaniabdos@gmail.com>
2024-03-22 21:40:23 +0000

    commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating file using puppet
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
