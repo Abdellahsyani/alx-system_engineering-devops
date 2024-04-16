aaa94278bb2115a815b538b5122caf4ce06409c1
Abdellahsyani <syaniabdos@gmail.com>
2024-04-16 19:09:34 +0100

    setup

---
+++ 0x14-mysql/setup_user.sql
@@ -0,0 +1,8 @@
+-- create a user and grant them priviliges
+  CREATE USER
+IF NOT EXISTS 'holberton_user'@'localhost'
+IDENTIFIED BY 'projectcorrection280hbtn';
+
+GRANT REPLICATION CLIENT
+   ON *.*
+   TO 'holberton_user'@'localhost';
