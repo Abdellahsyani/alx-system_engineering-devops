1d1b315e5135fabd37e10174d86ad1217c0da303
Abdellahsyani <syaniabdos@gmail.com>
2024-04-25 10:40:41 +0100

    try to fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
 """import data from API
 """
-import requests
-import sys
 
+from json import loads
+from requests import get
+from sys import argv
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(complete)) for complete in completed]
+
+    employee_id = argv[1]
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos/"\
+        .format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}"\
+        .format(employee_id)
+
+    response_todo = get(todo_url)
+    response_user = get(user_url)
+
+    list_emp = response_todo.json()
+    EMPLOYEE_NAME = response_user.json().get('name')
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = len(list_emp)
+    for i in list_emp:
+        if i.get('completed'):
+            NUMBER_OF_DONE_TASKS += 1
+
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks", end="")
+    print(f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
+    for i in list_emp:
+        if i.get('completed'):
+            print("\t {}".format(i.get('title')))
