5231a9abad2361f5b6010ea590d722091c002abe
Abdellahsyani <syaniabdos@gmail.com>
2024-04-25 10:14:28 +0100

    try to fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,14 +5,12 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        user = requests.get('https://jsonplaceholder.typicode.com'
-                            '/users?name' + sys.argv[1])
+        user = requests.get('https://jsonplaceholder.typicode.com/users/' + sys.argv[1])
         user = user.json()
-        tasks = requests.get('https://jsonplaceholder.typicode.com'
-                             '/todos?userId=' + sys.argv[1])
+        tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1])
         tasks = tasks.json()
         uncompleted = [t.get('title') for t in tasks if t.get('completed')]
         print('Employee {} is done with tasks({}/{}):'
-              .format(user[0].get('name'), len(uncompleted), len(tasks)))
+              .format(user.get('name'), len(uncompleted), len(tasks)))
         for task in uncompleted:
             print('\t ' + str(task))
