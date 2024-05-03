e754a21e7c38604a3092d16ad358a03ec975222d
Abdellahsyani <syaniabdos@gmail.com>
2024-05-03 19:04:40 +0100

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-"""import data from API
-"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url_user = requests.get("https://jsonplaceholder.typicode.com/users?id="
-                            + sys.argv[1])
-    url_todo = requests.get("https://jsonplaceholder.typicode.com"
-                            "/todos?userId=" + sys.argv[1])
-    user = url_user.json()
-    todo = url_todo.json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    EMPLOYEE_NAME = user[0].get('name')
-    NUMBER_OF_DONE_TASKS = sum(1 for i in todo if i.get('completed', True))
-    TOTAL_NUMBER_OF_TASKS = len(todo)
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
-                  TOTAL_NUMBER_OF_TASKS))
-    for i in todo:
-        if i.get('completed'):
-            print("\t {}".format(i.get('title')))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""exporting data gathered from an API to csv"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
-
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
@@ -11,6 +10,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
