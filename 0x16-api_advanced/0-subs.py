3f7eebf81cbf212328285bda9eb1312d243f8a80
Abdellahsyani <syaniabdos@gmail.com>
2024-05-07 16:28:21 +0100

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-
+"""get the number of subs"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Retrieve the number of subscribers for a given subreddit."""
+    """Get the number of subs to a subreddit"""
     url = "https://www.reddit.com/r/{sub}/about.json"
-    
-    headers = {"User-Agent": "My Reddit API Client"}
-    # Make the API request
-    response = requests.get(url=url.format(sub=subreddit), headers=headers)
-    
-    # Check if the response status code indicates success (200 OK)
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        # Extract the number of subscribers
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    else:
-        # If the subreddit is not found, return 0
+    headers = {
+        "User-Agent": "yahia:intranet.alxswe.com:v2.4.1 (by /u/keroxyz)",
+    }
+    res = requests.get(url=url.format(sub=subreddit), headers=headers)
+    if res.status_code != 200:
         return 0
+    data = res.json()["data"]
+    return data.get("subscribers", 0)
