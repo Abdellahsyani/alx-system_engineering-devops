e29bf2e3087848acbf2f4a5ce06fb81a8d04aa20
Abdellahsyani <syaniabdos@gmail.com>
2024-05-08 01:05:58 +0100

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Get the number of subs to a subreddit"""
     url = "https://www.reddit.com/r/{sub}/about.json"
     headers = {
-            "User-Agent": "MyApp/1.0",
+        "User-Agent": "abdellah:intranet.alxswe.com:v2.4.1 (by /u/keroxyz)",
     }
     res = requests.get(url=url.format(sub=subreddit), headers=headers)
     if res.status_code != 200:
