1271eaffed89cbe28186fd68f4df6f7a68bd583c
Abdellahsyani <syaniabdos@gmail.com>
2024-05-16 16:02:21 +0100

    request solved

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
-# executing a command
-exec { 'increase ULIMIT':
-  command => "sed -i 's/15/1024/g' nginx && service nginx restart",
-  cwd     => '/etc/default/',
-  path    => ['/usr/bin', '/bin']
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
